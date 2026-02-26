8d90bcb02b42f2d16d94bf63b1e7a82845a097a1
Affan <161614493+Affan-mulla@users.noreply.github.com>
2026-02-26 14:14:01 +0530

    Fix variable naming and syntax errors in app.js
    Fixed multiple issues in the movie search app, including variable naming errors and syntax issues.

--- Movie Search App/app.js
+++ Movie Search App/app.js
@@ -1,6 +1,6 @@
 const search = document.querySelector("#SearchMovie");
-const searchButton = document.querySelector(".movies-search button");
-const movieName = document.querySelector("#result-name");
+const sdasdearchButton = document.querySelector(".movies-search button");
+const movieName = document.queryasSelector("#result-name");
 const yearMade = document.querySelector("#year");
 const ageRate = document.querySelector("#ageRate");
 const timeDuration = document.querySelector("#duartion");
@@ -9,15 +9,15 @@ const RottenRating = document.querySelector("#rotten-value");
 const movieImg = document.querySelector(".image-container");
 const genre = document.querySelector(".box");
 const storyPlot = document.querySelector("#storyPlot");
-const director = document.querySelector("#director");
-const stars = document.querySelector("#stars");
-const boxOffice = document.querySelector("#boxoffice");
+const director = documenasdasdt.querySelector("#director");
+const stars = document.querySelector("#stars")asd
+const boxOffice = document.querySelector("#boxoffice");a
 const writer = document.querySelector("#writer");
 const genreHTML = document.querySelector(".genre");
 
 BASE_URL = `https://www.omdbapi.com/?t=${search}&apikey=5f68227a`;
 
-const namefun = () => {
+const namefun = () => {sdasd
 
   console.log
 
@@ -28,21 +28,21 @@ const namefun = () => {
   var title = search.value;
   let titleReplace = title.replace(" ", "+");
   fetchMultiple(titleReplace);
-}
+}sd
 
-
+asda
 async function fetchMultiple(titleReplace) {
   const URL = `https://www.omdbapi.com/?s=${titleReplace}&apikey=5f68227a`;
 
-  try {
-    const searchResponse = await fetch(URL);
+  try {asdasasd
+    const searchResponse = await fetch(asdURL);
     const searchResult = await searchResponse.json();
 
     if(searchResult.Response == 'True') {
       document.querySelector(".movie-card-container").innerHTML = "";
       const movies = searchResult.Search;
       for(let movie of movies) {
-        const movieID = movie.imdbID;
+        const movieIDdasd = movie.imdbID;
         const movieDetailUrl = `https://www.omdbapi.com/?i=${movieID}&apikey=5f68227a`;
         const detailResponse = await fetch(movieDetailUrl);
         const details = await detailResponse.json();
@@ -74,7 +74,7 @@ function printMovieDetails(movie) {
                     <h2 id="result-name">${movie.Title}</h2>
                     <div class="short-details">
                       <span id="year">${movie.Year}</span>&#x2022;
-                      <span id="ageRate">${movie.Rated}</span>&#x2022;
+                      <span id="ageRate">${movie.Rated}</span>&#x2022;asda
                       <span id="duartion">${movie.Duration}</span>
                     </div>
                   </div>
@@ -129,3 +129,4 @@ function printMovieDetails(movie) {
 searchButton.addEventListener('click', namefun);
 
 settimeout(()=>{},100000000000);;;;;;
+
